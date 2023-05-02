036e2b74cee3d4bbf5b73f757e374304bbf84fee
Illia Shulha <ilyxa2836@gmail.com>
2023-05-02 19:34:19 +0200

    bug fixes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import refs from './js/refs';
 import createMarkup from './js/createMarkup';
 import fetchData from './js/api';
 import { BASE_URL } from './js/api';
-let lightbox = new SimpleLightbox('.photo-card a');
 
 let inputValue = '';
 let currentPage = 1;
@@ -14,51 +13,59 @@ const imgPerPage = 40;
 refs.form.addEventListener('submit', onFormSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
-function onFormSubmit(e) {
+async function onFormSubmit(e) {
   e.preventDefault();
   refs.gallery.innerHTML = '';
 
   inputValue = e.currentTarget.elements.searchQuery.value;
-
-  fetchData(inputValue, imgPerPage, currentPage).then(
-    ({ data: { hits, totalHits } }) => {
-      if (!hits.length) {
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-      Notify.success(`"Hooray! We found ${totalHits} images."`);
-
-      if (hits.length === 40) {
-        setTimeout(() => (refs.loadMoreBtn.hidden = false), 1000);
-      }
+  try {
+    const {
+      data: { hits, totalHits },
+    } = await fetchData(inputValue, imgPerPage, currentPage);
+    if (!hits.length) {
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
     }
-  );
-}
 
-function onLoadMoreBtnClick() {
+    const galleryMarkup = createMarkup(hits);
+    refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+    Notify.success(`"Hooray! We found ${totalHits} images."`);
+    if (hits.length === 40) {
+      refs.loadMoreBtn.hidden = false;
+    }
+
+    const lightbox = new SimpleLightbox('.photo-card a');
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function onLoadMoreBtnClick() {
   currentPage += 1;
 
-  fetchData(inputValue, imgPerPage, currentPage).then(
-    ({ data: { hits, totalHits } }) => {
-      if (currentPage * imgPerPage >= totalHits) {
-        Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-        refs.loadMoreBtn.hidden = true;
-      }
-      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-      lightbox.refresh();
-
-      const { height: cardHeight } = document
-        .querySelector('.gallery')
-        .firstElementChild.getBoundingClientRect();
-      console.log(cardHeight);
-      scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
+  try {
+    const {
+      data: { hits, totalHits },
+    } = await fetchData(inputValue, imgPerPage, currentPage);
+    if (currentPage * imgPerPage >= totalHits) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      refs.loadMoreBtn.hidden = true;
     }
-  );
+
+    const galleryMarkup = createMarkup(hits);
+    refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+    lightbox.refresh();
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    console.log(cardHeight);
+    scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
